903ec64ea59e63f813b5538078205c31dc4cfa52
Ejiroghene Pinnock <epinnock@gmail.com>
2023-12-15 20:13:05 -0600

    added gemini lib & model.json and updated dropdown

--- src/common/ModelDropdown.tsx
+++ src/common/ModelDropdown.tsx
@@ -1,6 +1,7 @@
 import { Select } from '@chakra-ui/react';
 import React from 'react';
 import { useAppState } from '../state/store';
+import modelData from '../assets/json/models.json';
 
 const ModelDropdown = () => {
   const { selectedModel, updateSettings } = useAppState((state) => ({
@@ -20,16 +21,14 @@ const ModelDropdown = () => {
       value={selectedModel || ''}
       onChange={(e) => updateSettings({ selectedModel: e.target.value })}
     >
-      <option value='gpt-3.5-turbo'>GPT-3.5 Turbo</option>
-      <option value='gpt-3.5-turbo-16k'>GPT-3.5 Turbo (16k)</option>
-      <option value='gpt-4'>GPT-4</option>
-      <option value='gpt-4-1106-preview'>GPT-4 Turbo (128k)</option>
-      <option value='gpt-4-vision-preview'>GPT-4 Vision Turbo (128k)</option>
-      <option value='NurtureAI/OpenHermes-2.5-Mistral-7B-16k'>Open Hermes 2.5 (Local)</option>
-      <option value='microsoft/Orca-2-13b'>Orca 2</option>
-      <option value='TheBloke/agentlm-70B-AWQ'>AgentLM (Local)</option>
+      {modelData.map((model) => (
+        <option key={model.name} value={model.name}>
+          {model.displayName}
+        </option>
+      ))}
     </Select>
   );
 };
 
 export default ModelDropdown;
+
